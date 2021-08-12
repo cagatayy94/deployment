dcd8807c476ef43a1710c5a28804cde652a448da
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2021-08-12 18:48:08 +0300

    created repo

---
+++ src/test/java/com/continuous/integration/continuous/deployment/DeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuous.integration.continuous.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
